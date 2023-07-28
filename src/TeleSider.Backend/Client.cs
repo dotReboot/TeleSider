d1ec5ec2c4510b76e2f94568f6e9af3505fdde96
RainyArcher <74546799+RainyArcher@users.noreply.github.com>
2023-07-29 00:39:28 +0300

    The Android Sign In issue is resolved by using a custom logger.

--- src/TeleSider.Backend/Client.cs
+++ src/TeleSider.Backend/Client.cs
@@ -2,27 +2,28 @@
 using WTelegram;
 
 namespace BackEnd;
-public class Client
+public static class Client
 {
     private static WTelegram.Client client = null;
     public static string username = "";
     public static async Task Login(string phoneNumber)
     {
-        client = new WTelegram.Client(1, "1", "/data/data/com.telesider.telesider/files/session");
+        Helpers.Log = DebugLogger;
+        client = new WTelegram.Client(1, "1", "/data/data/com.telesider.telesider/files/TeleSider.session");
         await DoLogin(phoneNumber);
         //client.Dispose(); // the client must be disposed when you're done running your userbot.
     }
 
     public static async Task DoLogin(string loginInfo)
-        {
-            while (client.User == null)
-                switch (await client.Login(loginInfo))
-                {
-                    case "verification_code": return;
-                    case "password": return;
-                    default: loginInfo = null; break;
-                }
+    {
+        while (client.User == null)
+            switch (await client.Login(loginInfo))
+            {
+                case "verification_code": return;
+                case "password": return;
+                default: loginInfo = null; break;
+            }
         username = client.User.ToString();
-        }
-
+    }
+    private static void DebugLogger(int level, string message) => System.Diagnostics.Debug.WriteLine(message);
 }
